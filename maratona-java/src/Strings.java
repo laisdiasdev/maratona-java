f128a402f89fe3ca3b26506c517b3f07887d760d
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-09-15 16:48:50 -0300

    classe String

---
+++ maratona-java/src/Strings.java
@@ -0,0 +1,11 @@
+
+public class Strings {
+
+	public static void main(String[] args) {
+		// String não é um tipo primitivo de dados e sim uma classe Reference Type
+		// ela deve ser iniciada com letra maiúscula como tod classe
+
+		String nome = "Javeiros";
+	}
+
+}
