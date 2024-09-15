5f99b98b46a83ec8f70776f18c54685d5943b96a
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-09-15 18:18:40 -0300

    operadores aritmeticos

---
+++ maratona-java/src/OperadoresAritmeticos.java
@@ -0,0 +1,16 @@
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		// São eles:  + - / 
+		
+		int num1 = 40;
+		int num2 = 20;
+		double resultado = num1 / num2;
+		System.out.println(resultado);
+		
+		// Operador de resto: %
+		
+	}
+
+}
