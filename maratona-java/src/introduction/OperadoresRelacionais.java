75e5955f618090959cba821d15a2c206a0fa0ea4
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-09-15 18:41:15 -0300

    operadores relacionais

---
+++ maratona-java/src/introduction/OperadoresRelacionais.java
@@ -0,0 +1,20 @@
+package introduction;
+
+public class OperadoresRelacionais {
+	
+	public static void main(String[] args) {
+		// São eles: <, >, <=, >=, ==, !=
+		
+		boolean isDezMaiorQueVinte = 10 > 20;
+		boolean isDezMenorQueVinte = 10 < 20;
+		boolean isDezIgualVinte = 10==20;
+		boolean isDezDiferenteVinte = 10!=20;
+		
+		
+		System.out.println("10 é maior do que 20? "+isDezMaiorQueVinte);
+		System.out.println("10 é menor do que 20? "+isDezMenorQueVinte);
+		System.out.println("Dez é igual a 20? "+isDezIgualVinte);
+		System.out.println("Dez é diferente de 20? "+isDezDiferenteVinte);
+		
+	}
+}
