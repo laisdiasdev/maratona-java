7dacb2bf1940b1e448ce1d619e413f2fa287ca86
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-09-21 19:55:36 -0300

    operador ternário

--- maratona-java/src/estruturasCondicionais/OperadorTernario.java
+++ maratona-java/src/estruturasCondicionais/OperadorTernario.java
@@ -5,7 +5,7 @@ public class OperadorTernario {
 	public static void main(String[] args) {
 		// Fazer doações, se salário > 5000
 		double salario = 6000;
-		String mensagemDoar = "Eu vou dor 500 pra caridade";
+		String mensagemDoar = "Eu vou doar 500 pra caridade";
 		String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter";
 		
 		// Sintaxe de um operador ternário: (condicao) ? verdadeiro : falso
