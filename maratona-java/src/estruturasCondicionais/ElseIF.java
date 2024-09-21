0f659f98cd765027c925ccebf566bb5b5717ea9b
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-09-21 00:31:26 -0300

    condição if-else

---
+++ maratona-java/src/estruturasCondicionais/ElseIF.java
@@ -0,0 +1,20 @@
+package estruturasCondicionais;
+
+public class ElseIF {
+
+	public static void main(String[] args) {
+		
+		int nota1 = 5;
+		int nota2 = 9;
+		double media = (nota1 + nota2)/2;
+		
+		if (media >= 7) {
+			System.out.println("Aprovado!");
+		}else {
+			System.out.println("Reprovado");
+		}
+		
+
+	}
+
+}
