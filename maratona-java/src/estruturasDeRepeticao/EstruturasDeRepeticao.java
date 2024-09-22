454a7ce4ee72136de647ddad1027609e5bd7d981
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-09-21 21:24:33 -0300

    as 3 estruturas de repetição

--- maratona-java/src/estruturasDeRepeticao/EstruturasDeRepeticao.java
+++ maratona-java/src/estruturasDeRepeticao/EstruturasDeRepeticao.java
@@ -1,12 +1,24 @@
 package estruturasDeRepeticao;
 	
-public class While{
+public class EstruturasDeRepeticao{
 	public static void main(String[] args) {
 		
+		// Estrutura While
 		int count = 0;
 		while (count < 10) {
 			System.out.println(++count);
 		}
 		
+		// Estrutura Do-While
+		count = 0;
+		do {
+			System.out.println(" Dentro do do-while: "+ ++count);
+		}while (count < 10);
+		
+		// Estrutura For
+		// declara a var | define o range | faz a alteração
+		for(int i = 0; i < 10; i++) {
+			System.out.println("For "+i);
+		}
 	}
 }
