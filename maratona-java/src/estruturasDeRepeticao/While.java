a8afad69c23baf89a0a0c5434c41f0a340002694
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-09-21 21:06:27 -0300

    estrutura while

---
+++ maratona-java/src/estruturasDeRepeticao/While.java
@@ -0,0 +1,12 @@
+package estruturasDeRepeticao;
+	
+public class While{
+	public static void main(String[] args) {
+		
+		int count = 0;
+		while (count < 10) {
+			System.out.println(++count);
+		}
+		
+	}
+}
