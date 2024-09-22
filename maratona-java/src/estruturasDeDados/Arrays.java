e40c8234d45d01fff45f9694ed9137fd89d7ef94
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-09-22 14:38:43 -0300

    estrutura dados array

---
+++ maratona-java/src/estruturasDeDados/Arrays.java
@@ -0,0 +1,20 @@
+package estruturasDeDados;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		int[] idades = new int[3]; 
+		// no array em java, obrigatoriamente tem que declarar o tamanho do array
+		
+		idades[0]= 21;
+		idades[1]= 24;
+		idades[2]= 21;
+
+		System.out.println(idades[0]);
+		System.out.println(idades[1]);
+		System.out.println(idades[2]);
+		
+		
+	}
+
+}
