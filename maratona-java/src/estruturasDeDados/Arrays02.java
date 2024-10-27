81b8dd9a4507035b99e05876539bd6555e5877a2
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-10-27 20:04:17 -0300

    inicialização de arrays

---
+++ maratona-java/src/estruturasDeDados/Arrays02.java
@@ -0,0 +1,31 @@
+package estruturasDeDados;
+
+public class Arrays02 {
+
+	public static void main(String[] args) {
+		// Diferentes tpos de inicialização de arrays.
+		// byte, short, int, long, float e double são INICIALIZADOS COM 0
+		// char: ' ' (espaço ou unicode)
+		// boolean: false
+		// String: null
+		
+		String[] exemplo = new String[3];
+		System.out.println(exemplo[0]);
+		System.out.println(exemplo[1]);
+		System.out.println(exemplo[2]);
+		System.out.println("");
+		
+		boolean[] exemplo2 = new boolean[3];
+		System.out.println(exemplo2[0]);
+		System.out.println(exemplo2[1]);
+		System.out.println(exemplo2[2]);
+		System.out.println("");
+		
+		char[] exemplo3 = new char[3];
+		System.out.println(exemplo3[0]);
+		System.out.println(exemplo3[1]);
+		System.out.println(exemplo3[2]);
+
+	}
+
+}
