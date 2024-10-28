d7fae63f080961071b191ce5dc96f9fc679369bf
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-10-27 21:23:21 -0300

    imprimindo array

---
+++ maratona-java/src/estruturasDeDados/Array3.java
@@ -0,0 +1,20 @@
+package estruturasDeDados;
+
+public class Array3 {
+
+	public static void main(String[] args) {
+
+		String[] nomes = new String[3];
+		
+		nomes[0] = "Sam";
+		nomes[1] = "Clover";
+		nomes[2] = "Alex";
+		
+		for (int i = 0; i < nomes.length; i++) {
+			System.out.println(nomes[i]);
+		}
+
+		// EXISTEM OUTRAS MANEIRAS DE IMPRIMIR ARRAYS
+	}
+
+}
