54cd32ae09002dc93a85cae81055d297f5ab1c08
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-11-07 22:29:37 -0300

    teste de nomeclatura de pacotes

---
+++ maratona-java/src/orientacaoAObjetos/Pessoa.java
@@ -0,0 +1,7 @@
+package orientacaoAObjetos;
+
+public class Pessoa {
+	public String nome;
+	public int idade;
+	public char sexo;
+}
---
+++ maratona-java/src/orientacaoAObjetos/dominio/Pessoa.java
@@ -0,0 +1,7 @@
+package orientacaoAObjetos.dominio;
+
+public class Pessoa {
+	public String nome;
+	public int idade;
+	public char sexo;
+}
