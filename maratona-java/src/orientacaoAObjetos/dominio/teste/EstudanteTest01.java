511db4aad705bd0261eae1dabfb081293b95dcc2
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-11-07 22:53:13 -0300

    instanciando um objeto

---
+++ maratona-java/src/orientacaoAObjetos/dominio/teste/EstudanteTest01.java
@@ -0,0 +1,16 @@
+package orientacaoAObjetos.dominio.teste;
+
+import orientacaoAObjetos.dominio.Estudante;
+
+public class EstudanteTest01 {
+
+	public static void main(String[] args) {
+		Estudante estudante = new Estudante();
+		
+		estudante.nome = "Caio";
+		estudante.idade = 19;
+		estudante.sexo = 'M';
+
+	}
+
+}
