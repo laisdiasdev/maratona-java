43b6666200326920187f42beffe6f07eb62c85a9
Laís <113736391+laisdiasdev@users.noreply.github.com>
2024-11-08 23:38:54 -0300

    exercício

---
+++ maratona-java/src/orientacaoAObjetos/exercicios/package-info.java
@@ -0,0 +1,10 @@
+package orientacaoAObjetos.exercicios;
+
+/* 
+ Crie uma classe Carro com os seguintes atributos:
+ nome
+ modelo
+ ano
+ 
+ Em seguida, crie dois objetos distintos e imprima seus valores
+ */
